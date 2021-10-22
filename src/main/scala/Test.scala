ce4f2f125d7faf39832550efb846ac6bf20bdc34
Graham Hindle <grahamhindle@mac.com>
2021-10-22 17:19:47 +0200

    add version

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -3,6 +3,6 @@ object Test {
   def main(args: Array[String]): Unit = {
     println("Hello graham")
     println("testing git - now with git Kraken version 8.014")
-    println(" final test with macpro kraken")
+    println(" final test with macpro kraken 8.014")
   }
 }
